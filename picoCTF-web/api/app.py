af4129a50ff0f9bf67e6add7341ddfc00ae6b627
Ryan Riley <rriley.cs@gmail.com>
2018-05-20 12:01:32 +0300

    Fix to turn a False into a None.
    Flask 1.0+ bug loads config SESSION_COOKIE_DOMAIN correctly as None
    but later converts it to bool false.

--- picoCTF-web/api/app.py
+++ picoCTF-web/api/app.py
@@ -78,16 +78,18 @@ def after_request(response):
     response.headers.add('Cache-Control', 'no-cache')
     response.headers.add('Cache-Control', 'no-store')
     if api.auth.is_logged_in():
+        # Flask 1.0+ bug loads config SESSION_COOKIE_DOMAIN correctly as None
+        # but later converts it to bool false.
+        domain = app.config['SESSION_COOKIE_DOMAIN']
+        if not domain:
+            domain = None
+
         if 'token' in session:
-            response.set_cookie(
-                'token',
-                session['token'],
-                domain=app.config['SESSION_COOKIE_DOMAIN'])
+            response.set_cookie('token', session['token'], domain=domain)
         else:
             csrf_token = api.common.token()
             session['token'] = csrf_token
-            response.set_cookie(
-                'token', csrf_token, domain=app.config['SESSION_COOKIE_DOMAIN'])
+            response.set_cookie('token', csrf_token, domain=domain)
 
     # JB: This is a hack. We need a better solution
     if request.path[0:19] != "/api/autogen/serve/":
