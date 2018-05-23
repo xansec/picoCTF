8edd1ba5eedfa360ef27d6b5ed82a77527477b9c
Ivan <r33t@cmu.edu>
2018-05-23 13:32:56 -0400

    Pin Flask < 1.0

--- picoCTF-web/setup.py
+++ picoCTF-web/setup.py
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["Flask", "flask-bcrypt", "pymongo==3.2.1", "pyzmq", "py",
+    install_requires=["Flask==0.12.2", "flask-bcrypt", "pymongo==3.2.1", "pyzmq", "py",
                       "pytest", "voluptuous", "gunicorn", "spur", "line_profiler",
                       "Flask-Mail"],
 
