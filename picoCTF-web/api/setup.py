7856dfc84c262a6ab02fa0f113c63d8f049275b1
Ivan <r33t@cmu.edu>
2018-09-29 19:56:18 -0400

    Update mongodb indexes based on high-frequency query usage
    (cherry picked from commit 0e3ddb7, f3a23ab)

--- picoCTF-web/api/setup.py
+++ picoCTF-web/api/setup.py
@@ -18,21 +18,28 @@ def index_mongo():
 
     db.users.ensure_index("uid", unique=True, name="unique uid")
     db.users.ensure_index("username", unique=True, name="unique username")
+    db.users.ensure_index("tid")
 
     db.groups.ensure_index("gid", unique=True, name="unique gid")
+
     db.problems.ensure_index("pid", unique=True, name="unique pid")
 
-    db.submissions.ensure_index("tid", name="submission tids")
-    db.ssh.ensure_index("tid", unique=True, name="unique ssh tid")
+    db.submissions.ensure_index([("tid", 1), ("uid", 1), ("correct", 1)])
+    db.submissions.ensure_index([("uid", 1), ("correct", 1)])
+    db.submissions.ensure_index([("tid", 1), ("correct", 1)])
+    db.submissions.ensure_index([("pid", 1), ("correct", 1)])
+    db.submissions.ensure_index("uid")
+    db.submissions.ensure_index("tid")
+
     db.teams.ensure_index("team_name", unique=True, name="unique team names")
+    db.teams.ensure_index("country")
 
     db.shell_servers.ensure_index("name", unique=True, name="unique shell name")
     db.shell_servers.ensure_index("sid", unique=True, name="unique shell sid")
 
     db.cache.ensure_index("expireAt", expireAfterSeconds=0)
     db.cache.ensure_index("kwargs", name="kwargs")
-    db.cache.ensure_index("kwargs.uid", name="kwargs.uid")
-    db.cache.ensure_index("kwargs.tid", name="kwargs.tid")
+    db.cache.ensure_index([("function", 1), ("ordered_kwargs", 1)])
     db.cache.ensure_index("args", name="args")
 
     db.shell_servers.ensure_index(
