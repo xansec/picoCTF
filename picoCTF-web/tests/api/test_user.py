0ccf149d0e244c43a3f23e6290989bffd2d46632
Dustin Martin <dustin@dmartin.io>
2019-04-01 14:14:10 -0400

    Finish status and user creation tests

--- picoCTF-web/tests/api/test_user.py
+++ picoCTF-web/tests/api/test_user.py
@@ -1,9 +1,11 @@
 """Tests for the /api/user/ routes."""
+# noqa: E712
 import pytest
 import json
 import pymongo
 
 from api import api
+from api.auth import confirm_password
 
 TESTING_DB_NAME = 'ctf_test'
 db = None
@@ -43,14 +45,33 @@ def client():
 
 # User route tests
 def test_status(client):
-    """Check that the /status route returns the expected results."""
+    """
+    Check that the /status route returns the expected results.
+
+    Expected results based on a newly initialized DB.
+    """
     res = client.get('/api/user/status')
-    #@todo check for more fields
+    status, message, data = decode_response(res)
     assert res.status_code == 200
+    assert data['logged_in'] is False
+    assert data['admin'] is False
+    assert data['teacher'] is False
+    assert data['enable_teachers'] is True
+    assert data['enable_feedback'] is True
+    assert data['enable_captcha'] is False
+    assert data['competition_active'] is False
+    assert data['username'] == ''
+    assert data['tid'] == ''
+    assert data['email_verification'] is False
 
 
 def test_create_user(client):
-    """Test user creation."""
+    """
+    Test user creation.
+
+    Ensures that the specified user is created in the database.
+    @todo separate tests for student, teacher, admin, other user creation
+    """
     clear_db()
     db = get_conn()
     assert db.get_collection('users').find({"username": "sampleuser"}) \
@@ -77,5 +98,21 @@ def test_create_user(client):
     assert status == 1
     assert message == "User \'{}\' registered successfully!".format(
         'sampleuser')
-    assert db.get_collection('users').find({"username": "sampleuser"}) \
-             .count() == 1
+    user_db_entries = list(db.get_collection('users').find(
+        {"username": "sampleuser"}))
+    assert len(user_db_entries) == 1
+    db_user = user_db_entries[0]
+    assert db_user['firstname'] == 'First'
+    assert db_user['lastname'] == 'Last'
+    assert db_user['username'] == 'sampleuser'
+    assert db_user['email'] == 'sample@example.com'
+    assert confirm_password('samplepw', db_user['password_hash'])
+    assert db_user['usertype'] == 'teacher'
+    assert db_user['country'] == 'US'
+    assert db_user['demo']['parentemail'] == 'sample@example.com'
+    assert db_user['demo']['age'] == '18+'
+    assert db_user['teacher'] is True
+    assert db_user['admin'] is True
+    assert db_user['eligible'] is False
+    assert db_user['verified'] is True
+
