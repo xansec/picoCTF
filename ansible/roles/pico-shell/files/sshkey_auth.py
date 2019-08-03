325e7261c7cc92ff92996cb743bba9d4b622b0c5
Ivan <r33t@cmu.edu>
2019-08-02 21:45:00 -0400

    Checks for valid user before accepting ssh auth key
    Resolves #239

---
+++ ansible/roles/pico-shell/files/sshkey_auth.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import json
+import pwd
+import sys
+from os import path
+
+import requests
+
+LOCAL_ROOT = "/opt/hacksports/local/"
+
+config_file = path.join(LOCAL_ROOT, "local_config.json")
+config = json.loads(open(config_file).read())
+SERVER = config["web_server"]
+LIMIT_BYPASS = config['rate_limit_bypass']
+TIMEOUT = 5
+
+
+def competition_active():
+    r = requests.get(SERVER + "/api/v1/status")
+    return json.loads(r.text)["competition_active"]
+
+
+def try_login(user):
+    r = requests.post(
+        SERVER + "/api/v1/user/login",
+        headers={
+            "user-agent": 'picoCTF Shell Server',
+            'Limit-Bypass': LIMIT_BYPASS
+        },
+        json={
+            "username": user,
+            "password": "-"
+        },
+        timeout=TIMEOUT)
+    return str(json.loads(r.text)['message'])
+
+
+def server_valid_user(user):
+    if not competition_active():
+        return False
+    result = try_login(user)
+    return result == "Incorrect password"
+
+
+def main(user):
+    if not server_valid_user(user):
+        exit(0)
+    else:
+        auth_keys_dir = path.join(pwd.getpwnam(user).pw_dir, '.ssh')
+        auth_keys_file = path.join(auth_keys_dir, 'authorized_keys')
+        if path.exists(auth_keys_file):
+            with open(auth_keys_file, 'r') as fin:
+                print(fin.read())
+        else:
+            exit(0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("No user specified")
+        sys.exit("Bad args")
+    else:
+        main(sys.argv[1])
