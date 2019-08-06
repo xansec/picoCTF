50ad63635b0de6b96ebd0065bec0cdd379d8a64f
Dustin Martin <dustin@dmartin.io>
2019-08-06 11:57:15 -0400

    Update eligibility recalculation endpoint

--- picoCTF-web/api/apps/v1/teams.py
+++ picoCTF-web/api/apps/v1/teams.py
@@ -80,26 +80,45 @@ class Team(Resource):
 @ns.response(401, 'Not logged in')
 @ns.response(403, 'Permission denied')
 @ns.response(404, 'Team not found')
-@ns.route('/<string:team_id>/recalculate_eligibility')
-class RecalculateEligibilityResponse(Resource):
-    """Force recalculation of a team's eligibility status."""
+@ns.route('/<string:team_id>/recalculate_eligibilities')
+class RecalculateEligibilitiesResponse(Resource):
+    """Force recalculation of a team's eligibilities."""
 
     @require_admin
     def get(self, team_id):
         """
-        Force recalculation of a team's eligibility status.
+        Force recalculation of a team's eligibilities.
 
-        Once a team has been found to be ineligible once, they are permanently
-        flagged as such. This admin-only endpoint can potentially reverse this,
-        if the competition's eligiblity conditions have been updated or the
-        member(s) previously causing ineligiblity have been deleted.
+        Re-evaluate a team's eligibilities. May be useful if a former team
+        member that caused a team to become ineligible for an event has
+        deleted their account, or if a new event has been added since the
+        team was formed.
         """
         team = api.team.get_team(team_id)
         if not team:
             raise PicoException('Team not found', 404)
-        eligibility = api.team.is_eligible(team_id)
-        api.team.mark_eligiblity(team_id, eligibility)
+
+        team_members = api.team.get_team_members(
+            tid=team_id, show_disabled=False)
+        all_events = api.events.get_all_events()
+        member_eligibilities = dict()
+        for member in team_members:
+            member_eligibilities[member['uid']] = {
+                event['eid'] for event in all_events
+                if api.events.is_eligible(member, event)
+            }
+
+        team_eligibilities = list(
+            set.intersection(*member_eligibilities.values()))
+        db = api.db.get_conn()
+        db.teams.find_one_and_update(
+            {"tid": team_id},
+            {"$set": {
+                "eligibilities": team_eligibilities
+            }}
+        )
+
         return jsonify({
             'success': True,
-            'eligible': eligibility
+            'eligibilities': team_eligibilities
         })
