5b3bfdc365498e51e8300ddc367a7f091363131a
Dustin Martin <dustin@dmartin.io>
2019-08-26 11:45:26 -0400

    Fail registration for various reasons

--- picoCTF-web/tests/load/registration.py
+++ picoCTF-web/tests/load/registration.py
@@ -28,7 +28,14 @@ def generate_user():
     }
     return user_fields
 
+def register_and_expect_failure(l, user_demographics):
+    with l.client.post(REGISTRATION_ENDPOINT,
+            json=user_demographics, catch_response=True) as res:
+        if res.status_code == 400:
+            res.success()
+
 class RegistrationTasks(TaskSet):
+    """Tasks for both successful and failed registrations."""
 
     @task(weight=10)
     def successfully_register(l):
@@ -37,14 +44,75 @@ class RegistrationTasks(TaskSet):
         raise StopLocust # Terminate after successful registration
 
     @task(weight=1)
-    def registration_error(l):
-        user_demographics = generate_user()
-        user_demographics['username'] = ''
-        with l.client.post(REGISTRATION_ENDPOINT,
-                json=user_demographics, catch_response=True) as res:
-            if res.status_code == 400:
-                res.success()
+    class RegistrationErrorTasks(TaskSet):
+        """Tasks which fail registration for various reasons."""
 
+        @task
+        def missing_field_error(l):
+            user_demographics = generate_user()
+            to_delete = random.choice([
+                'username', 'password', 'email', 'affiliation', 'country', 'usertype', 'demo'
+            ])
+            del user_demographics[to_delete]
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def username_error(l):
+            user_demographics = generate_user()
+            user_demographics['username'] = ''
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def password_error(l):
+            user_demographics = generate_user()
+            user_demographics['password'] = 'ooo'
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def email_error(l):
+            user_demographics = generate_user()
+            user_demographics['email'] = 'invalid_email_address'
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def affiliation_error(l):
+            user_demographics = generate_user()
+            user_demographics['affiliation'] = ''
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def country_error(l):
+            user_demographics = generate_user()
+            user_demographics['country'] = 'xx'
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def usertype_error(l):
+            user_demographics = generate_user()
+            user_demographics['usertype'] = 'invalid_user_type'
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def demo_error(l):
+            user_demographics = generate_user()
+            user_demographics['demo']['age'] = 'invalid_age'
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
+
+        @task
+        def require_parent_email_error(l):
+            user_demographics = generate_user()
+            user_demographics['demo']['age'] = '13-17'
+            user_demographics['demo']['parentemail'] = ''
+            register_and_expect_failure(l, user_demographics)
+            l.interrupt()
 
 class RegistrationLocust(HttpLocust):
     task_set = RegistrationTasks
