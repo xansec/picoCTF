c1df96e59752f706a0eb8d977c258e08f0edb9a8
Dustin Martin <dustin@dmartin.io>
2019-08-26 16:56:55 -0400

    Move shared code into util file, load users from DB

--- picoCTF-web/tests/load/registration.py
+++ picoCTF-web/tests/load/registration.py
@@ -4,42 +4,12 @@ import random
 
 from locust import HttpLocust, task, TaskSet
 from locust.exception import StopLocust
-from pymongo import MongoClient
 
 from demographics_generator import (get_affiliation, get_country_code,
                                     get_demographics, get_email, get_password,
                                     get_user_type, get_username)
+from util import get_db, REGISTRATION_ENDPOINT
 
-db = None
-db_conf = {
-    'MONGO_HOST': '127.0.0.1',
-    'MONGO_PORT': 27017,
-    'MONGO_USER': None,
-    'MONGO_PASS': None,
-    'MONGO_DB_NAME': 'picoCTF-load-testing',
-}
-
-API_BASE_URL = 'api/v1'
-REGISTRATION_ENDPOINT = API_BASE_URL + '/users'
-
-def get_db():
-    global db
-    if not db:
-        if db_conf['MONGO_USER'] or db_conf['MONGO_PASS']:
-            mongo_client = MongoClient(
-                    "mongodb://{}:{}@{}:{}/{}?authMechanism=SCRAM-SHA-1".format(
-                        db_conf['MONGO_USER'], db_conf['MONGO_PASS'],
-                        db_conf['MONGO_HOST'], db_conf['MONGO_PORT'],
-                        db_conf['MONGO_DB_NAME']
-                    ))
-        else:
-            mongo_client = MongoClient(
-                "mongodb://{}:{}/{}".format(
-                    db_conf['MONGO_HOST'], db_conf['MONGO_PORT'],
-                    db_conf['MONGO_DB_NAME']
-                ))
-        db = mongo_client[db_conf['MONGO_DB_NAME']]
-    return db
 
 def generate_user():
     """Generate a set of valid demographics for the given user type."""
---
+++ picoCTF-web/tests/load/util.py
@@ -0,0 +1,38 @@
+"""Handles MongoDB connections for load testing."""
+
+from pymongo import MongoClient
+
+db = None
+db_conf = {
+    'MONGO_HOST': '127.0.0.1',
+    'MONGO_PORT': 27017,
+    'MONGO_USER': None,
+    'MONGO_PASS': None,
+    'MONGO_DB_NAME': 'picoCTF-load-testing',
+}
+
+API_BASE_URL = 'api/v1'
+REGISTRATION_ENDPOINT = API_BASE_URL + '/users'
+LOGIN_ENDPOINT = API_BASE_URL + '/user/login'
+LOGOUT_ENDPOINT = API_BASE_URL + '/user/logout'
+
+SHELL_PAGE_URL = 'shell'
+
+def get_db():
+    global db
+    if not db:
+        if db_conf['MONGO_USER'] or db_conf['MONGO_PASS']:
+            mongo_client = MongoClient(
+                    "mongodb://{}:{}@{}:{}/{}?authMechanism=SCRAM-SHA-1".format(
+                        db_conf['MONGO_USER'], db_conf['MONGO_PASS'],
+                        db_conf['MONGO_HOST'], db_conf['MONGO_PORT'],
+                        db_conf['MONGO_DB_NAME']
+                    ))
+        else:
+            mongo_client = MongoClient(
+                "mongodb://{}:{}/{}".format(
+                    db_conf['MONGO_HOST'], db_conf['MONGO_PORT'],
+                    db_conf['MONGO_DB_NAME']
+                ))
+        db = mongo_client[db_conf['MONGO_DB_NAME']]
+    return db
