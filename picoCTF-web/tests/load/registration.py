44d996a545d24ace7e34d285b87c8d87cce48128
Dustin Martin <dustin@dmartin.io>
2019-08-26 14:53:56 -0400

    Store created load testing users in db

--- picoCTF-web/tests/load/registration.py
+++ picoCTF-web/tests/load/registration.py
@@ -1,19 +1,45 @@
 """Register various types of users and store their login credentials."""
 
 import random
-import uuid
-from locust import HttpLocust, TaskSet, task
+
+from locust import HttpLocust, task, TaskSet
 from locust.exception import StopLocust
+from pymongo import MongoClient
 
-from demographics_generator import get_affiliation, get_country_code, get_user_type, get_username, get_password, get_email, get_demographics
+from demographics_generator import (get_affiliation, get_country_code,
+                                    get_demographics, get_email, get_password,
+                                    get_user_type, get_username)
 
-MONGO_HOST = "127.0.0.1"
-MONGO_PORT = 27017
-MONGO_USER = None
-MONGO_PASS = None
+db = None
+db_conf = {
+    'MONGO_HOST': '127.0.0.1',
+    'MONGO_PORT': 27017,
+    'MONGO_USER': None,
+    'MONGO_PASS': None,
+    'MONGO_DB_NAME': 'picoCTF-load-testing',
+}
 
 API_BASE_URL = 'api/v1'
 REGISTRATION_ENDPOINT = API_BASE_URL + '/users'
+
+def get_db():
+    global db
+    if not db:
+        if db_conf['MONGO_USER'] or db_conf['MONGO_PASS']:
+            mongo_client = MongoClient(
+                    "mongodb://{}:{}@{}:{}/{}?authMechanism=SCRAM-SHA-1".format(
+                        db_conf['MONGO_USER'], db_conf['MONGO_PASS'],
+                        db_conf['MONGO_HOST'], db_conf['MONGO_PORT'],
+                        db_conf['MONGO_DB_NAME']
+                    ))
+        else:
+            mongo_client = MongoClient(
+                "mongodb://{}:{}/{}".format(
+                    db_conf['MONGO_HOST'], db_conf['MONGO_PORT'],
+                    db_conf['MONGO_DB_NAME']
+                ))
+        db = mongo_client[db_conf['MONGO_DB_NAME']]
+    return db
 
 def generate_user():
     """Generate a set of valid demographics for the given user type."""
@@ -40,6 +66,8 @@ class RegistrationTasks(TaskSet):
     @task(weight=10)
     def successfully_register(l):
         user_demographics = generate_user()
+        get_db().users.insert_one(user_demographics.copy())
+
         l.client.post(REGISTRATION_ENDPOINT, json=user_demographics)
         raise StopLocust # Terminate after successful registration
 
