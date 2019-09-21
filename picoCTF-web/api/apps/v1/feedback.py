69b54ee4238e4c1ebf5f5404261e5da13e79f89f
Ivan <r33t@cmu.edu>
2019-09-21 08:44:25 -0400

    Fix feedback route returns all by default for admin, add `/all`
    Trim feedback output to only required fields

--- picoCTF-web/api/apps/v1/feedback.py
+++ picoCTF-web/api/apps/v1/feedback.py
@@ -4,7 +4,7 @@ from flask_restplus import Namespace, Resource
 
 import api
 from api import (block_before_competition, check_csrf, PicoException,
-                 require_login)
+                 require_admin, require_login)
 
 from .schemas import feedback_list_req, feedback_submission_req
 
@@ -22,34 +22,16 @@ class FeedbackList(Resource):
     @ns.response(401, 'Not logged in')
     @ns.response(403, 'Not authorized')
     @ns.response(422, 'Competition has not started')
-    @ns.expect(feedback_list_req)
     def get(self):
         """
         Get the list of your submitted problem feedback.
-
-        If called as an admin, returns all submitted feedback, with optional
-        filtering using the ?pid, ?uid, and ?tid querystring arguments.
         """
-        curr_user = api.user.get_user()
-        req = feedback_list_req.parse_args(strict=True)
-
-        # Handle args if they are present but unset
-        for arg in ['pid', 'uid', 'tid']:
-            if req[arg] == '':
-                req[arg] = None
-
-        # Non-admins can only view their own feedback
-        if not curr_user.get('admin', False):
-            for arg in ['pid', 'uid', 'tid']:
-                if req[arg] is not None:
-                    raise PicoException(
-                        'You must be an admin to filter the feedback list',
-                        status_code=403)
-            req['uid'] = curr_user['uid']
-
-        return jsonify(api.problem_feedback.get_problem_feedback(
-            pid=req['pid'], tid=req['tid'], uid=req['uid']
-        ))
+        raw_feedback = api.problem_feedback.get_problem_feedback(
+            uid=api.user.get_user()['uid']
+        )
+        return jsonify([
+            {"feedback": f['feedback'], "pid": f['pid']} for f in raw_feedback
+        ])
 
     @check_csrf
     @block_before_competition
@@ -82,3 +64,35 @@ class FeedbackList(Resource):
         })
         res.status_code = 201
         return res
+
+
+@ns.route('/all')
+class FeedbackList(Resource):
+    """Get the list of problem feedback, or submit new feedback."""
+
+    @block_before_competition
+    @require_admin
+    @require_login
+    @ns.response(200, 'Success')
+    @ns.response(400, 'Error parsing request')
+    @ns.response(401, 'Not logged in')
+    @ns.response(403, 'Not authorized')
+    @ns.response(422, 'Competition has not started')
+    @ns.expect(feedback_list_req)
+    def get(self):
+        """
+        Get the list of your submitted problem feedback.
+
+        If called as an admin, returns all submitted feedback, with optional
+        filtering using the ?pid, ?uid, and ?tid querystring arguments.
+        """
+        req = feedback_list_req.parse_args(strict=True)
+
+        # Handle args if they are present but unset
+        for arg in ['pid', 'uid', 'tid']:
+            if req[arg] == '':
+                req[arg] = None
+
+        return jsonify(api.problem_feedback.get_problem_feedback(
+            pid=req['pid'], tid=req['tid'], uid=req['uid']
+        ))
