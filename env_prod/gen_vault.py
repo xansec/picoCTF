f0e9118ec7a02d2a9cd80d9432a396b7483e4268
Roy Ragsdale <roy@ragsdale.xyz>
2020-06-02 19:36:58 -0400

    Update automation for env_prod
    - uses streamlined minimal variables list
    - comments inventory
    - adds task documentation

---
+++ env_prod/gen_vault.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+VAULT_TEMPLATE = """
+# web
+vault_flask_app_secret_key            :
+vault_flask_app_rate_limit_bypass_key :
+
+# shell
+vault_shell_manager_deploy_secret     :
+
+# db
+vault_picoAdmin_db_password           :
+vault_picoWeb_db_password             :
+vault_redis_db_password               :
+"""
+
+def main():
+
+if __name__ == '__main__':
+    main()
